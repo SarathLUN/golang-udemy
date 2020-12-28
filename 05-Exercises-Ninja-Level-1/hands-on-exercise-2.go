827cdcde935f4850f6cec718f498f600937d869f
TONY <tony@TONYs-MacBook-Pro.local>
2020-12-29 00:22:37 +0700

    done Hands-on-exercise-2

---
+++ 05-Exercises-Ninja-Level-1/hands-on-exercise-2.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	// a. print out the values for each identifier
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	// b. The compiler assigned values to the variables. What are these values called?
+	// => this is called Zero Value
+}
