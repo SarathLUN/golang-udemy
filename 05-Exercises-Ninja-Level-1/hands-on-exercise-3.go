ef2b6d2bdad4ad69351c4a0c16886875ca51bb38
TONY <tony@TONYs-MacBook-Pro.local>
2020-12-29 14:04:32 +0700

    change variable name to avoide conflict in package scope

--- 05-Exercises-Ninja-Level-1/hands-on-exercise-3.go
+++ 05-Exercises-Ninja-Level-1/hands-on-exercise-3.go
@@ -2,14 +2,14 @@ package main
 
 import "fmt"
 
-var x = 42
-var y = "James Bond"
-var z = true
+var i = 42
+var s = "James Bond"
+var b = true
 
 func main() {
 	// a. use fmt.Sprintf to print all of the VALUES to one single string. ASSIGN the returned value of TYPE string using the short declaration operator to a VARIABLE with the IDENTIFIER “s”
-	s := fmt.Sprintf("%v\t%v\t%v",x,y,z)
+	r := fmt.Sprintf("%v\t%v\t%v", i, s, b)
 
 	// b. print out the value stored by variable “s”
-	fmt.Println(s)
+	fmt.Println(r)
 }
