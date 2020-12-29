2dceaae5c5918c0523e7fd4c11b1818ec73ca036
TONY <tony@TONYs-MacBook-Pro.local>
2020-12-29 22:18:31 +0700

    using var with datatype

--- 06-Programming-fundamentals/number-type.go
+++ 06-Programming-fundamentals/number-type.go
@@ -2,11 +2,14 @@ package main
 
 import "fmt"
 
+var x2 int64
+var y float32
+
 func main() {
-	x := 43
-	y := 43.5895764
-	fmt.Println(x)
+	x2 = 43
+	y = 43.5895764
+	fmt.Println(x2)
 	fmt.Println(y)
-	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", x2)
 	fmt.Printf("%T\n", y)
 }
