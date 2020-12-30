44f53a7449c85016da3aaa81b83f7c3d1ccf62e5
TONY <tony@TONYs-MacBook-Pro.local>
2020-12-30 11:56:11 +0700

    done video video: 028 Constant

---
+++ 06-Programming-fundamentals/constants.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// un-type constant
+const (
+	a = 64
+	b = 43.46
+	c = "Tony"
+)
+
+// typed constant
+const (
+	d int     = 87
+	e float64 = 98.46
+	f string  = "Hello world."
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+	fmt.Println(b)
+	fmt.Printf("%T\n", b)
+	fmt.Println(c)
+	fmt.Printf("%T\n", c)
+}
