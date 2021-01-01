feb0093b71ecee786c90245d61ea0e6c57ff6dd6
TONY <tony@TONYs-MacBook-Pro.local>
2021-01-01 10:53:04 +0700

    done Exercises - Ninja Level 2, Hands-on exercise #2

---
+++ 07-Exercises-Ninja-Level-2/hands-on-exercise-2.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 1 == 2
+	fmt.Println(a)
+	a = 1 <= 2
+	fmt.Println(a)
+	a = 1 >= 2
+	fmt.Println(a)
+	a = 1 != 2
+	fmt.Println(a)
+	a = 1 < 2
+	fmt.Println(a)
+	a = 1 > 2
+	fmt.Println(a)
+
+}
