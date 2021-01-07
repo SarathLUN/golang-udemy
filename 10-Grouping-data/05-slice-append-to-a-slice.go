96789f8360b226ad87b39f02d1add2838c81c867
TONY <tony@TONYs-MacBook-Pro.local>
2021-01-07 10:54:09 +0700

    finish video 64 Slice - append to a slice

---
+++ 10-Grouping-data/05-slice-append-to-a-slice.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{3, 4, 6, 34, 46}
+	fmt.Println(x)
+
+	x = append(x, 123, 456)
+	fmt.Println(x)
+
+	y := []int{77, 88, 99}
+	x = append(x, y...) // "..." = unpack sth
+	fmt.Println(x)
+}
