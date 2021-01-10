828340e6226f503ade1f4759b8af304f89132412
TONY <tony@TONYs-MacBook-Pro.local>
2021-01-10 12:16:04 +0700

    finish Exercises - Ninja Level 4, Hands-on exercise #10, solution

---
+++ 11-Exercises-Ninja-Level-4/exercise-10.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string][]string{
+		`bond_james`:      {`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: {`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           {`Being evil`, `Ice cream`, `Sunsets`},
+	}
+	m[`tony`] = []string{`test1`, `test2`} //add new record to the map
+	delete(m, `no_dr`)                     // delete record from map
+	for k, v1 := range m {
+		fmt.Println("This is the record for ", k)
+		for i, v2 := range v1 {
+			fmt.Printf("\t index-%d = %v \n", i, v2)
+		}
+	}
+}
