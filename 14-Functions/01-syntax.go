d267cba6fb33bbe85c474154d5d8aa7c03f5eaa5
TONY <tony@TONYs-MacBook-Pro.local>
2021-01-17 20:18:33 +0700

    done func with multiple returns

--- 14-Functions/01-syntax.go
+++ 14-Functions/01-syntax.go
@@ -12,6 +12,17 @@ func main() {
 	//return
 	result := woo(1, 2)
 	fmt.Println(result)
+
+	//multiple returns
+	x, y := mouse("Ian", "Fleming")
+	fmt.Println(x)
+	fmt.Println(y)
+}
+
+func mouse(fn string, ln string) (string, bool) {
+	a := fmt.Sprint(fn, " ", ln, `, says "Hello"`)
+	b := true
+	return a, b
 }
 
 func woo(i int, j int) int {
