33b1e1e33bafd1e12580e61f6c9035c87dcfc074
TONY <tony@TONYs-MacBook-Pro.local>
2021-01-23 20:24:21 +0700

    - finish video: 101 - Recursion

---
+++ 14-Functions/12-recursion.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	n1 := factorial(4)
+	fmt.Println(n1)
+
+	n2 := loopFact(4)
+	fmt.Println(n2)
+}
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func loopFact(n int) int {
+	total := 1
+	for ; n > 0; n-- {
+		total *= n
+	}
+	return total
+}
