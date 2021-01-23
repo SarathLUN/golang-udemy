d297c6fe2a8090ab7bc8cb861662fc728a447f7f
TONY <tony@TONYs-MacBook-Pro.local>
2021-01-23 12:49:18 +0700

    - add experimentation on multiple defer in a function.

--- 14-Functions/04-defer.go
+++ 14-Functions/04-defer.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	defer foo2() // will execute at the last of function
+	defer bar3() // defer will run back in order(last in first out)
 	bar2()
 }
 
@@ -13,4 +14,8 @@ func foo2() {
 
 func bar2() {
 	fmt.Println("bar2")
+}
+
+func bar3() {
+	fmt.Println("bar3")
 }
