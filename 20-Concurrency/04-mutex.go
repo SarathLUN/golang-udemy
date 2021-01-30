562963d297cb3015c1a10bb88d2c3244d9dce6ed
TONY <tony@TONYs-MacBook-Pro.local>
2021-01-30 18:04:40 +0700

    - finish video: 135 - Mutex

---
+++ 20-Concurrency/04-mutex.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	fmt.Println("CPU:", runtime.NumCPU())
+	fmt.Println("Goroutine:", runtime.NumGoroutine())
+
+	counter := 0
+	const gs = 100
+	var wg sync.WaitGroup
+	wg.Add(gs)
+	var mu sync.Mutex
+	for i := 0; i < gs; i++ {
+		go func() {
+			mu.Lock()
+			v := counter
+			runtime.Gosched()
+			v++
+			counter = v
+			mu.Unlock()
+			wg.Done()
+		}()
+		fmt.Println("Goroutine:", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("Goroutine:", runtime.NumGoroutine())
+	fmt.Println("counter:", counter)
+}
