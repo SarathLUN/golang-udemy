57c84b68ea6515a6cc808252783ce6190a847b2b
TONY <tony@TONYs-MacBook-Pro.local>
2021-01-31 11:54:34 +0700

    - finish Exercises - Ninja Level 9 - Hands-on exercise #5

--- 21-Exercise-Ninja-Level-09/05-exercise-05.go
+++ 21-Exercise-Ninja-Level-09/05-exercise-05.go
@@ -9,14 +9,12 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
-
 	var incrementer int64
 
 	gs := 100
 	wg.Add(gs)
 	for i := 0; i < gs; i++ {
 		go func() {
-
 			atomic.AddInt64(&incrementer, 1)
 			fmt.Println(atomic.LoadInt64(&incrementer))
 			fmt.Println("goroutine:", runtime.NumGoroutine())
@@ -26,5 +24,3 @@ func main() {
 	wg.Wait()
 	fmt.Println("end value:", incrementer)
 }
-
-// run with ->go run -race 03-exercise-03.go
