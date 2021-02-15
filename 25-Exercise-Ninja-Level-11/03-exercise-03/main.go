9dc7e92d30532d86ef789f0ad6d9df296e43c4ef
TONY <tony@TONYs-MacBook-Pro.local>
2021-02-15 22:14:42 +0700

    - done Exercises - Ninja Level 11 Hands-on exercise #3

---
+++ 25-Exercise-Ninja-Level-11/03-exercise-03/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type customErr struct {
+	info string
+}
+
+func (c customErr) Error() string {
+	return fmt.Sprintf("here is erro: %v", c.info)
+}
+
+func main() {
+	c1 := customErr{
+		info: "need more coffee",
+	}
+	foo(c1)
+}
+
+func foo(e error) {
+	fmt.Println("foo ran -", e, "\n")
+
+}
