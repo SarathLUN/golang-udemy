9540e467fbc2f72e38cb93ef6cc6b6522bb7bc34
TONY <tony@TONYs-MacBook-Pro.local>
2021-02-27 21:02:48 +0700

    - finish video 184 - Golint

--- 27-Testing-Benchmarking/03-example-test/acdc/main_test.go
+++ 27-Testing-Benchmarking/03-example-test/acdc/main_test.go
@@ -2,11 +2,11 @@ package acdc
 
 import "fmt"
 
-func ExampleSum_01() {
+func ExampleSum_first() {
 	fmt.Println(Sum(2, 3))
 	// Output: 5
 }
-func ExampleSum_02() {
+func ExampleSum_second() {
 	fmt.Println(Sum(2, 3, 4))
 	// Output:
 	// 9
