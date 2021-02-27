434ec7ad6524833c85cf79ec7b50387a68f71029
TONY <tony@TONYs-MacBook-Pro.local>
2021-02-27 14:36:53 +0700

    - proof the function name with lower case - worked :=).

--- 27-Testing-Benchmarking/01-introduction/main.go
+++ 27-Testing-Benchmarking/01-introduction/main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("2 + 3 =", MySum(2, 3))
-	fmt.Println("4 + 7 =", MySum(4, 7))
-	fmt.Println("5 + 9 =", MySum(5, 9))
+	fmt.Println("2 + 3 =", mySum(2, 3))
+	fmt.Println("4 + 7 =", mySum(4, 7))
+	fmt.Println("5 + 9 =", mySum(5, 9))
 }
 
-func MySum(xi ...int) int {
+func mySum(xi ...int) int {
 	sum := 0
 	for _, v := range xi {
 		sum += v
--- 27-Testing-Benchmarking/01-introduction/main_test.go
+++ 27-Testing-Benchmarking/01-introduction/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestMySum(t *testing.T) {
-	x := MySum(2, 3)
+	x := mySum(2, 3)
 	if x != 5 {
 		t.Error("Expected", 5, "Got", x)
 	}
